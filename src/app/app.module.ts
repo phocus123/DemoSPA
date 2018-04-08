7af880e239f89719116760fad1997159fa4a3268
Mark Schafers <mark_schafers@hotmail.com>
2018-04-08 13:39:18 +0800

    added home and register components and navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
+import { FormsModule } from '@angular/forms';
+import { BsDropdownModule } from 'ngx-bootstrap';
 
 import { AppComponent } from './app.component';
-import { ValueComponent } from './components/value/value.component';
+import { HomeComponent } from './components/home/home.component';
+import { NavComponent } from './components/nav/nav.component';
+import { AuthService } from './services/auth.service';
+import { RegisterComponent } from './components/register/register.component';
 
 @NgModule({
-  declarations: [AppComponent, ValueComponent],
-  imports: [BrowserModule, HttpModule],
-  providers: [],
+  declarations: [AppComponent, HomeComponent, NavComponent, RegisterComponent],
+  imports: [BrowserModule, HttpModule, FormsModule, BsDropdownModule.forRoot()],
+  providers: [AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
